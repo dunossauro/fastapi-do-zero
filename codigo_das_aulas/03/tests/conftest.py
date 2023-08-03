3a8ba006e81f219877ece05f7f4080415f9515ce
dunossauro <mendesxeduardo@gmail.com>
2023-08-03 03:04:56 -0300

    [REV] Revisão de tópicos na aula 03
    - Remoção da explicação das fixtures (mudaram para aula 02)
    - Simplificação da fixture de session
    - Atualização dos slides

--- codigo_das_aulas/03/tests/conftest.py
+++ codigo_das_aulas/03/tests/conftest.py
@@ -1,19 +1,21 @@
 import pytest
+from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
+from fast_zero.app import app
 from fast_zero.models import Base
 
 
 @pytest.fixture
+def client():
+    return TestClient(app)
+
+
+@pytest.fixture
 def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
-    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    engine = create_engine('sqlite:///:memory:')
+    Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     yield Session()
     Base.metadata.drop_all(engine)
