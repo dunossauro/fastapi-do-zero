028961afc617cbf5e66db1020b5b18eac9724a82
dunossauro <mendesxeduardo@gmail.com>
2023-10-25 19:09:41 -0300

    [WIP] Adição do prefixo 'auth' no router da aula 06
    relates #32

--- codigo_das_aulas/06/tests/conftest.py
+++ codigo_das_aulas/06/tests/conftest.py
@@ -55,7 +55,7 @@ def user(session):
 @pytest.fixture
 def token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     return response.json()['access_token']
--- codigo_das_aulas/06/tests/test_auth.py
+++ codigo_das_aulas/06/tests/test_auth.py
@@ -1,6 +1,6 @@
 def test_get_token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     token = response.json()
