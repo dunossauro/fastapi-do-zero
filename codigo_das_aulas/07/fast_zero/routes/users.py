1c91faecbf2b3c825448ea142da7924eeac05cbe
dunossauro <mendesxeduardo@gmail.com>
2023-12-20 03:33:51 -0300

    Ajustando aulas posteriores a tag de routers
    closes #70

--- codigo_das_aulas/07/fast_zero/routes/users.py
+++ codigo_das_aulas/07/fast_zero/routes/users.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -8,11 +10,13 @@ from fast_zero.routes.auth import get_current_user
 from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
 from fast_zero.security import get_password_hash
 
-router = APIRouter(prefix='/users')
+router = APIRouter(prefix='/users', tags=['users'])
+Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/', response_model=UserPublic, status_code=201)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
+def create_user(user: UserSchema, session: Session):
     db_user = session.scalar(select(User).where(User.email == user.email))
     if db_user:
         raise HTTPException(status_code=400, detail='Email already registered')
@@ -32,7 +36,7 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 @router.get('/', response_model=UserList)
 def read_users(
-    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+    session: Session, skip: int = 0, limit: int = 100
 ):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
     return {'users': users}
@@ -42,8 +46,8 @@ def read_users(
 def update_user(
     user_id: int,
     user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(status_code=400, detail='Not enough permissions')
@@ -65,8 +69,8 @@ def update_user(
 @router.delete('/{user_id}', response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(status_code=400, detail='Not enough permissions')
