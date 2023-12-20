770621d0562949a903b82e5a82e6a0787c9f7d0a
dunossauro <mendesxeduardo@gmail.com>
2023-12-20 03:41:21 -0300

    Correção na porta dos entrypoints 8888 -> 8000
    closes #71

--- codigo_das_aulas/10/entrypoint.sh
+++ codigo_das_aulas/10/entrypoint.sh
@@ -2,5 +2,4 @@
 
 poetry run alembic upgrade head
 
-# Perguntar a entendedores!
-poetry run uvicorn --host 0.0.0.0 --port 8888 fast_zero.app:app
+poetry run uvicorn --host 0.0.0.0 --port 8000 fast_zero.app:app
