ad62b9e0f2f992f705e3c413df587eda619e4e1e
dunossauro <mendesxeduardo@gmail.com>
2024-02-01 10:13:42 -0300

    Adicionando códigos da aula 03 ao repositório

---
+++ codigo_das_aulas/03/fast_zero/app.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+app = FastAPI()
+
+
+@app.get('/', status_code=200, response_model=Message)
+def read_root():
+    return {'message': 'Olá Mundo!'}
+
+
+database = []  # Lista provisória para fins de estudo
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
