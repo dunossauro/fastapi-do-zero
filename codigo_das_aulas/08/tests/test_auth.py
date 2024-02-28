bcaea6740a89b47199f0ddceeced8f83e840f8d8
dunossauro <mendesxeduardo@gmail.com>
2024-02-28 17:32:20 -0300

    Ajustes para o pyjwt na aula 08
    Adicionando ExpiredSignatureError
    
    related #102
    related #67

--- codigo_das_aulas/08/tests/test_auth.py
+++ codigo_das_aulas/08/tests/test_auth.py
@@ -61,3 +61,26 @@ def test_token_expiry(client, user):
         )
         assert response.status_code == 401
         assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time('2023-07-14 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == 200
+        token = response.json()['access_token']
+
+    with freeze_time('2023-07-14 12:31:00'):
+        response = client.put(
+            f'/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'username': 'wrongwrong',
+                'email': 'wrong@wrong.com',
+                'password': 'wrong',
+            },
+        )
+        assert response.status_code == 401
+        assert response.json() == {'detail': 'Could not validate credentials'}
