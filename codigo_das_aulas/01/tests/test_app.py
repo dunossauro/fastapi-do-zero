5b0882b6f60ed11b4af75ca886352c563bf951da
dunossauro <mendesxeduardo@gmail.com>
2024-04-03 12:50:11 -0300

    Adapatação do cópdigo da aula 01 para texto da aula 01
    - Warnings no pytest(#122)
    - Revisão (#67)
    - Compatibilidade com python 3.12 (#118)
    - Status codes (#120)

--- codigo_das_aulas/01/tests/test_app.py
+++ codigo_das_aulas/01/tests/test_app.py
@@ -1,12 +1,14 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
 def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app)
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/')
+    response = client.get('/')  # Act
 
-    assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
