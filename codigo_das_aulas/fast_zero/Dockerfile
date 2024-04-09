090ffdd25514a3c43d54ef56e9f539e914412d71
dunossauro <mendesxeduardo@gmail.com>
2024-04-09 15:23:09 -0300

    Updates na aula 10
    - Modificações do ruff
    - Update 3.12
    
    realted #67

--- codigo_das_aulas/fast_zero/Dockerfile
+++ codigo_das_aulas/fast_zero/Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11-slim
+FROM python:3.12-slim
 ENV POETRY_VIRTUALENVS_CREATE=false
 
 WORKDIR app/
---
+++ codigo_das_aulas/fast_zero/fast_zero/routers/todos.py
@@ -0,0 +1,109 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fast_zero.database import get_session
+from fast_zero.models import Todo, User
+from fast_zero.schemas import (
+    Message,
+    TodoList,
+    TodoPublic,
+    TodoSchema,
+    TodoUpdate,
+)
+from fast_zero.security import get_current_user
+
+router = APIRouter()
+
+Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+router = APIRouter(prefix='/todos', tags=['todos'])
+
+
+@router.post('/', response_model=TodoPublic)
+def create_todo(
+    todo: TodoSchema,
+    user: CurrentUser,
+    session: Session,
+):
+    db_todo: Todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=user.id,
+    )
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
+
+
+@router.get('/', response_model=TodoList)
+def list_todos(  # noqa
+    session: Session,
+    user: CurrentUser,
+    title: str = Query(None),
+    description: str = Query(None),
+    state: str = Query(None),
+    offset: int = Query(None),
+    limit: int = Query(None),
+):
+    query = select(Todo).where(Todo.user_id == user.id)
+
+    if title:
+        query = query.filter(Todo.title.contains(title))
+
+    if description:
+        query = query.filter(Todo.description.contains(description))
+
+    if state:
+        query = query.filter(Todo.state == state)
+
+    todos = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {'todos': todos}
+
+
+@router.patch('/{todo_id}', response_model=TodoPublic)
+def patch_todo(
+    todo_id: int, session: Session, user: CurrentUser, todo: TodoUpdate
+):
+    db_todo = session.scalar(
+        select(Todo).where(Todo.user_id == user.id, Todo.id == todo_id)
+    )
+
+    if not db_todo:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Task not found.'
+        )
+
+    for key, value in todo.model_dump(exclude_unset=True).items():
+        setattr(db_todo, key, value)
+
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
+
+
+@router.delete('/{todo_id}', response_model=Message)
+def delete_todo(todo_id: int, session: Session, user: CurrentUser):
+    todo = session.scalar(
+        select(Todo).where(Todo.user_id == user.id, Todo.id == todo_id)
+    )
+
+    if not todo:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Task not found.'
+        )
+
+    session.delete(todo)
+    session.commit()
+
+    return {'message': 'Task has been deleted successfully.'}
