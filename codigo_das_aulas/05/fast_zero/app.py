938d3a472c54f92fa1e7661e84854ae8e8f8b6fb
dunossauro <mendesxeduardo@gmail.com>
2024-05-15 18:44:13 -0300

    Adicionando validação de campos unique durante a criação de User
    Related #144
    
    Muito obrgiado @k-dash pela visão <3

--- codigo_das_aulas/05/fast_zero/app.py
+++ codigo_das_aulas/05/fast_zero/app.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import select
+from sqlalchemy import or_, select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
@@ -19,18 +19,27 @@ def read_root():
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = session.scalar(
-        select(User).where(User.username == user.username)
+        select(User).where(
+            or_(User.username == user.username, User.email == user.email)
+        )
     )
 
     if db_user:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
-            detail='Username already registered',
-        )
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
     db_user = User(
         username=user.username, password=user.password, email=user.email
     )
+
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
@@ -50,7 +59,6 @@ def read_users(
 def update_user(
     user_id: int, user: UserSchema, session: Session = Depends(get_session)
 ):
-
     db_user = session.scalar(select(User).where(User.id == user_id))
     if not db_user:
         raise HTTPException(
