b43d19b9dd74af0ab36218e25e619b75fa1b8a9a
dunossauro <mendesxeduardo@gmail.com>
2024-06-04 21:33:11 -0300

    Adicionando bullets no código de teste da aula 04

--- aulas/codigos/04/tests/test_db.py
+++ aulas/codigos/04/tests/test_db.py
@@ -5,9 +5,9 @@ from fast_zero.models import User
 
 def test_create_user(session):
     new_user = User(username='alice', password='secret', email='teste@test')
-    session.add(new_user)
-    session.commit()
+    session.add(new_user)#(1)!
+    session.commit()#(2)!
 
-    user = session.scalar(select(User).where(User.username == 'alice'))
+    user = session.scalar(select(User).where(User.username == 'alice'))#(3)!
 
     assert user.username == 'alice'
