5be797862fd36f62c6edf690dce98a39f30f50d8
dunossauro <mendesxeduardo@gmail.com>
2025-02-26 18:53:51 -0300

    Adicionando logger ao código fonte da aula 04

--- codigo_das_aulas/04/fast_zero/app.py
+++ codigo_das_aulas/04/fast_zero/app.py
@@ -33,6 +33,7 @@ def read_users():
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
+        logger.info(f'User with {user_id=} not found in the database')
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
