01bcfa9ff898f271fab216d35a0482d14f579ce5
dunossauro <mendesxeduardo@gmail.com>
2025-09-17 12:23:39 -0300

    Deployed 2e5c6e49 to 4.2.0 with MkDocs 1.6.1 and mike 2.1.3

---
+++ 4.2.0/codigos/03/fast_zero/app.py
@@ -0,0 +1,95 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post('/users/')
+def create_user():
+    ...
+
+
+from http import HTTPStatus
+
+# ...
+
+@app.post('/users/', status_code=HTTPStatus.CREATED)
+def create_user():
+    ...
+
+
+from fast_zero.schemas import Message, UserSchema
+
+# ...
+
+@app.post('/users/', status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    ...
+
+
+from fast_zero.schemas import Message, UserPublic, UserSchema
+
+# Código omitido
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    ...
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
+
+
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
+
+# código omitido
+
+database = []  #(1)!
+
+# código omitido
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)  #(2)!
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+# código omitido
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+from fastapi import FastAPI, HTTPException
+
+# ...
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1: #(1)!
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        ) #(2)!
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id #(3)!
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
